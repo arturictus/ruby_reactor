b2364b70713c5d3992869a0373b5c5d845c46b6f
Artur <1930175+arturictus@users.noreply.github.com>
2025-11-12 23:57:03 +0000

    setup default max_attempts to 0"

--- lib/ruby_reactor/worker.rb
+++ lib/ruby_reactor/worker.rb
@@ -49,57 +49,5 @@ module RubyReactor
       Sidekiq.logger.error("RubyReactor infrastructure failure: #{exception.message}")
       Sidekiq.logger.error("Job details: #{msg.inspect}")
     end
-
-    # TODO: delete from here --------------------------------
-    def run_compensation_for_current_step(context)
-      return unless context.current_step
-
-      step_config = context.reactor_class.steps[context.current_step]
-      return unless step_config
-
-      # Get the arguments that were passed to the failed step
-      arguments = resolve_arguments_for_step(step_config, context)
-
-      # Run compensation
-      compensate_step(step_config, context.retry_context.failure_reason, arguments, context)
-    end
-
-    def resolve_arguments_for_step(step_config, context)
-      # Simplified argument resolution for compensation
-      arguments = {}
-      step_config.arguments.each do |arg_name, arg_config|
-        arguments[arg_name] = resolve_argument_value(arg_config, context)
-      end
-      arguments
-    end
-
-    def resolve_argument_value(arg_config, context)
-      source = arg_config[:source]
-      case source
-      when RubyReactor::Template::Input
-        context.get_input(source.name)
-      when RubyReactor::Template::Result
-        context.get_result(source.step_name, source.path)
-      when RubyReactor::Template::Value
-        source.value
-      else
-        source
-      end
-    end
-
-    def compensate_step(step_config, error, arguments, context)
-      if step_config.compensate_block
-        step_config.compensate_block.call(error, arguments, context)
-      elsif step_config.has_impl?
-        step_config.impl.compensate(error, arguments, context)
-      else
-        RubyReactor.Success() # Default compensation
-      end
-    end
-
-    def log_unexpected_error(error, context)
-      Sidekiq.logger.error("RubyReactor unexpected error: #{error.message}")
-      Sidekiq.logger.error("Context: #{context.inspect}") if context
-    end
   end
 end
