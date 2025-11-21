1209dfd3b42edebf9570c8a169ed8789eac17b21
Artur <1930175+arturictus@users.noreply.github.com>
2025-11-21 00:28:50 +0000

    feat: Add test and `MultipleComposeRootReactor` to verify state merging with multiple inline composed reactors.

--- spec/nested_reactor_inline_execution_spec.rb
+++ spec/nested_reactor_inline_execution_spec.rb
@@ -18,4 +18,21 @@ RSpec.describe "Nested Reactor Inline Execution" do
     expect(result.value[:child_process][:async_step]).to eq("async_done_123")
     expect(result.value[:prepare]).to eq("prepared")
   end
+
+  it "correctly merges state when running inline with multiple composed reactors" do
+    # Mock async_router to run inline and return the executor
+    allow(RubyReactor.configuration.async_router).to receive(:perform_async) do |serialized_context, reactor_class_name|
+      # Simulate inline execution
+      worker = RubyReactor::Worker.new
+      worker.perform(serialized_context, reactor_class_name)
+    end
+
+    result = Support::MultipleComposeRootReactor.run(id: "root")
+
+    expect(result).to be_a(RubyReactor::Success)
+    expect(result.value[:first_step]).to eq("first_step_done")
+    expect(result.value[:child_1][:async_step]).to eq("async_done_child_1")
+    expect(result.value[:child_2][:async_step]).to eq("async_done_child_2")
+    expect(result.value[:last_step]).to eq("last_step_done")
+  end
 end
--- spec/support/nested_inline_reactors.rb
+++ spec/support/nested_inline_reactors.rb
@@ -21,4 +21,31 @@ module Support
       argument :id, input(:id)
     end
   end
+
+  class MultipleComposeRootReactor < RubyReactor::Reactor
+    input :id
+
+    step :first_step do
+      run { |_, _| RubyReactor::Success("first_step_done") }
+    end
+
+    compose :child_1, NestedInlineChildReactor do
+      argument :id, value("child_1")
+    end
+
+    compose :child_2 do
+      input :id
+
+      step :async_step do
+        async true
+        run { |args, _| RubyReactor::Success("async_done_#{args[:id]}") }
+      end
+
+      argument :id, value("child_2")
+    end
+
+    step :last_step do
+      run { |_, _| RubyReactor::Success("last_step_done") }
+    end
+  end
 end
