182a2e84d1ab52d31bf7a8ba357ef67c733b14c7
Artur <1930175+arturictus@users.noreply.github.com>
2025-12-27 02:18:27 +0100

    feat: track parent context for map elements and store child context IDs

--- spec/map/async_map_execution_spec.rb
+++ spec/map/async_map_execution_spec.rb
@@ -49,5 +49,15 @@ RSpec.describe "Async Map Execution" do
 
     expect(context).not_to be_nil
     expect(context["intermediate_results"]["doubled_numbers"]).to eq([2, 4, 6])
+
+    # Verify that children have parent_context_id
+    map_id = "#{context_id}:doubled_numbers"
+    child_context_ids = storage.retrieve_map_element_context_ids(map_id, MapTestReactors::AsyncMapReactor.name)
+    expect(child_context_ids).not_to be_empty
+
+    child_context_ids.each do |child_id|
+      child_context = storage.find_context_by_id(child_id)
+      expect(child_context["parent_context_id"]).to eq(context_id)
+    end
   end
 end
