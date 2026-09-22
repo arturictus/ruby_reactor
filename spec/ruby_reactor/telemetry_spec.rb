67a52910370abf31b78b764477a6b6de8fafad20
Artur <1930175+arturictus@users.noreply.github.com>
2026-09-22 21:27:40 +0100

    chore: update opentelemetry require path and bump gem versions in Gemfile.lock

--- spec/ruby_reactor/telemetry_spec.rb
+++ spec/ruby_reactor/telemetry_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "spec_helper"
-require "opentelemetry-sdk"
+require "opentelemetry/sdk"
 require "sidekiq/testing"
 
 # Define inline test steps and reactors
